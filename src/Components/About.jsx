f4980146fcfeb9cb946dbbee832586e2ce4983c8
Edwinz28 <edwin.zhang28@gmail.com>
2020-08-15 14:18:58 -0400

    changed favicon

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,11 +5,7 @@ class About extends Component {
   render() {
 
     if(this.props.data){
-      var profilepic= "/images/"+this.props.data.image;
-
-      console.log(this.props.data.image);
       var bio = this.props.data.bio;
-      var resumeDownload = this.props.data.resumedownload;
     }
 
     return (
