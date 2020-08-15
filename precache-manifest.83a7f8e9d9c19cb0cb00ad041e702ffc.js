f61d21bf2ee887615f63b07a3e28a20a7138c91b
Edwinz28 <edwin.zhang28@gmail.com>
2020-08-14 20:26:21 -0400

    Updates

---
+++ precache-manifest.83a7f8e9d9c19cb0cb00ad041e702ffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72ef5f5f42212a6404a1082273e9786",
+    "url": "/My-Site/index.html"
+  },
+  {
+    "revision": "a35785356ad69d502f5b",
+    "url": "/My-Site/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ea103c709be959cd65e2",
+    "url": "/My-Site/static/js/2.5d6984d2.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/My-Site/static/js/2.5d6984d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35785356ad69d502f5b",
+    "url": "/My-Site/static/js/main.e62807e7.chunk.js"
+  },
+  {
+    "revision": "6b4b2c13cf48b700d7ba",
+    "url": "/My-Site/static/js/runtime-main.4cd0907c.js"
+  }
+]);
