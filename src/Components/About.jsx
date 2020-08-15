d493a89cfd690b5411551ec92b93b6bb7b2d9545
Edwinz28 <edwin.zhang28@gmail.com>
2020-08-15 12:22:15 -0400

    fixed bug where profile pic did not show

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,6 +6,8 @@ class About extends Component {
 
     if(this.props.data){
       var profilepic= "/images/"+this.props.data.image;
+
+      console.log(this.props.data.image);
       var bio = this.props.data.bio;
       var resumeDownload = this.props.data.resumedownload;
     }
@@ -14,7 +16,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="" />
+            <img className="profile-pic"  src="images/profilepic.JPG" alt="" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
